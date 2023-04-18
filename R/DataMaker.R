86878be13da41583cc9fcce26faab938c8f84231
salmonm02 <myrrandansalmon@gmail.com>
2023-04-18 00:56:57 -0600

    Added in code that adds in the county of each store. Added in a new function that combines the existing data to the raw grocery data table.

--- R/DataMaker.R
+++ R/DataMaker.R
@@ -1,9 +1,27 @@
 #get the original data from the csv file provided
 
-create_data <- function(file){
-  x<- read.csv(file)
-  x
+create_data <- function(grocery_file, county_file){
+  #reads in geojson with all of the grocery data
+  groceries <- st_read(grocery_file)
+  groceries$county <- ""
+  
+  #reads in the shape file with all of the county boundary information
+  boundaries <- st_read(county_file)
+  
+  #finds the county associated with each store
+  g_geom <- groceries %>% select(geometry) %>% st_as_sf()
+  b_geom <- boundaries %>% select(geometry) %>% st_as_sf()
+  
+  for(x in 1:length(b_geom$geometry)){
+    for(y in 1:length(g_geom$geometry)){
+      if(st_intersects(g_geom[y,1], b_geom[x,1], sparse = FALSE)==TRUE){
+        groceries$county[y] <- boundaries$NAME[x]
+      }
+    }
+  }
+  groceries
 }
+
 
 existing_data <- function(file1, file2, file3){
   x <- read.csv(file1)
@@ -13,6 +31,51 @@ existing_data <- function(file1, file2, file3){
   a <- rbind(x, y, z)
   a <- subset(a, select = -c(X))
   a
+}
+
+combine_data <- function(data, exist_data){
+  #start with prepping the grocery stores data
+  data <- as.data.frame(data)
+  for(x in 1:length(data$CITY)){
+    if(grepl("WEST VALLEY CITY", data$CITY[x], fixed = TRUE)== TRUE){
+      data <- data[-x,]
+    }
+    if(grepl("SAN JUAN", data$county[x], fixed = TRUE)== TRUE){
+      data <- data[-x,]
+    }
+    if(grepl("UTAH", data$county[x], fixed = TRUE)== TRUE){
+      data <- data[-x,]
+    }
+  }
+  data$registers <- NA
+  data$selfchecko <- NA
+  data$total_registers <- NA
+  data$availability <- NA
+  data$cost <- NA
+  data$market <- NA
+  data$brand <- NA
+  data$Name <- data$NAME
+  data$type <- data$TYPE
+  
+  data <- data %>% select(Name, type, county, registers, selfchecko, total_registers,
+                          availability, cost, market, brand)
+  
+  #now for the data that already exists
+  exist_data$county <- ""
+  for(x in 1:length(exist_data$id)){
+    if(grepl("UT", exist_data$id[x], fixed = TRUE) == TRUE){
+      exist_data$county[x] <- "UTAH"
+    } else if(grepl("SJ", exist_data$id[x], fixed = TRUE) == TRUE){
+      exist_data$county[x] <- "SAN JUAN"
+    } else if(grepl("SL", exist_data$id[x], fixed = TRUE) == TRUE){
+    exist_data$county[x] <- "SALT LAKE"
+    }
+  }
+  exist_data <- exist_data %>% select(Name, type, county, registers, selfchecko, total_registers,
+                                      availability, cost, market, brand)
+  
+  combined_data <- rbind(data, exist_data)
+  combined_data
 }
 
 impute_data <- function(data, acquired_data){
@@ -29,7 +92,7 @@ impute_data <- function(data, acquired_data){
   #include the data from the acquired data.frame tp create one large data frame 
   #with missign values for unknown store attributes
   
-  #segment the data into three sub frames that correspond with counties that are
+  #segment the data into three sub data sets that correspond with counties that are
   #similar to salt lake, utah, and san juan
   
   ###PROBLEM
